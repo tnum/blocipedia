9b31aa03e5d7c8e756785b827a2204a40ace2c60
Teenum Applegate <teenum@Teenums-iMac.local>
2015-01-02 23:16:04 +0000

    seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create users
+
+15.times do 
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10) 
+  )
+  user.skip_confirmation!
+  user.save  
+end
+users = User.all
+
+# Create wikis
+50.times do
+  wiki = Wiki.create(
+    user:  users.sample,
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph
+  )
+  wiki.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+end
+
+wikis = Wiki.all
+
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save
+
+
+
