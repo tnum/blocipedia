e68e219ee2a00eeb7f195f99c9fc1b3a5ed8621d
tnum <t.chudha@gmail.com>
2015-01-27 16:00:02 +0000

    upgrading account specs passing

--- spec/features/user_upgrades_account_spec.rb
+++ spec/features/user_upgrades_account_spec.rb
@@ -16,18 +16,22 @@ feature 'User upgrades account' do
     expect(page).to have_content('premium')
   end
 
+  scenario 'for a year' do
+    # user who upgraded 11 months ago
+    # gets reminder to upgrade account for next year
+  end
+
   scenario 'can not make payment once premium' do
     upgrade_account
     visit('/charges/new')
     expect(current_path).to eq(root_path)
+    expect(page).to have_content('You are already a premium member')
+  end
+
+  scenario 'is downgraded if their membership expires' do
+    # user upgraded 12 months ago
+    # does not upgrade their account
+    # is downgraded to stadard membership
   end
 
 end
-
-# User signs up for a free plan
-
-# User upgrades their free plan and is prompted to pay with Stripe
-
-# User's role is changed from standard (free) to premium
-
-# User is able to create private wikis
